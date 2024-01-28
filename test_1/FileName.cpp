462408f60fdee5f119a4797a9686d3edab4253c4
s-Sampan <157936954+s-Sampan@users.noreply.github.com>
2024-01-28 11:37:56 +0800

    Add files via upload

---
+++ test_1/FileName.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int a = 1003;
+int main()
+{
+	printf("ƒ√ƒÛ¡À\n");
+	printf("%d\n", 114);
+	printf("%d\n", sizeof(char));
+	printf("%d\n", sizeof(short)); 
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(long));
+	printf("%d\n", sizeof(long long));
+	printf("%d\n", sizeof(float));
+	printf("%d\n", sizeof(double));
+	int age = 18;
+	double weight = 57.9;
+	float dd=18;
+	dd = dd - 13;
+	age = age + 1;
+	weight = weight-2.9;
+	int a = 120;
+	printf("%d\n",age);
+	printf("%lf\n", weight);
+	printf("%f\n",dd);
+	printf("%d\n",a);
+
+	return 0;
+}
